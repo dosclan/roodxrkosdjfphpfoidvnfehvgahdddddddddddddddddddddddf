6783ea51e9c71f019e89a93586f52f8cc286bda5
roooooooooooooodx <42224407+roooooooooooooodx@users.noreply.github.com>
2018-08-30 00:53:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1502,20 +1502,14 @@ m.sendMessage(args)
 
 
 client.on('message', message => {
-   if (message.content === "b-id") {
-   let embed = new Discord.RichEmbed()
-  .setColor("RANDOM")
-  .setThumbnail(message.author.avatarURL)
-  .addField("Name:",`${message.author.username}`, true)
-  .addField('Discrim:',"#" +  message.author.discriminator, true)
-  .addField("ID:", message.author.id, true)
-  .addField("Create At:", message.author.createdAt, true)
-     
-     
-  message.channel.sendEmbed(embed);
+    if (message.content === "b-id") {
+    let embed = new Discord.RichEmbed()
+   .setColor("RANDOM")
+   .setDescription(`** :id: : ${message.author.id} , ${message.author.username} **`)
+   message.channel.sendEmbed(embed);
     }
-});
- 
+  
+     });
  
  
 
